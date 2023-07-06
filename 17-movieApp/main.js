56b0d3d8effbe0a8944e3a6050db3da99b0c3ba0
belkisarslan <belkisarslan33@gmail.com>
2023-07-06 14:45:04 +0300

    tried again

--- 17-movieApp/main.js
+++ 17-movieApp/main.js
@@ -1,5 +1,5 @@
 const API_URL =
-  'https:api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
@@ -16,7 +16,7 @@ async function getMovies(url){
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  //console.log(data.results)
   showMovies(data.results)
 }
 
